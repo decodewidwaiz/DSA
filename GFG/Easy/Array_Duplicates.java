ffe77a9adde688f6a9c1ba8659e9b85fbfd6deec
Waiz <164471173+decodewidwaiz@users.noreply.github.com>
2025-08-24 13:32:42 +0530

    Add solution: undefined (GfgSoln)

---
+++ GFG/Easy/Array_Duplicates.java
@@ -0,0 +1,23 @@
+
+
+
+
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        ArrayList<Integer> dup = new ArrayList<>();
+        
+        // Iterate through the array and find duplicates
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                // Check if the current element is a duplicate and hasn't been added yet
+                if (dup.isEmpty() || dup.get(dup.size() - 1) != arr[i]) {
+                    dup.add(arr[i]);
+                }
+            }
+        }
+        
+        return dup;
+    }
+}
